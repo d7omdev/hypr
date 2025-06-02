a9ba5700c35abf879d05eb1e86b1e6b3bdea501d
d7omdev <abdohamdy198461@gmail.com>
2025-06-02 15:52:38 +0300

    update

--- scripts/input.sh
+++ scripts/input.sh
@@ -2,4 +2,4 @@ input=$(
 	hyprctl devices -j | jq -r '.keyboards[] | select(.main == true) | .active_keymap | if . | test("English") then "En" else if . | test("Arabic") then "Ar" else "Unknown" end end'
 )
 
-echo " $input "
+echo "  $input "
